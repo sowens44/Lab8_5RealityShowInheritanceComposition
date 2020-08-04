c9ed9555b3b14ab04a6ede38e488e9bd43fb667e
so2owens@gmail.com <so2owens@gmail.com>
2020-08-04 00:26:15 -0400

    Add project files.

---
+++ HackersApplication.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab8_5RealityShowInheritanceComposition
+{
+    class HackersApplication : ContestantApplication
+    {
+
+        #region Properties
+
+        private int CodingExperienceYears { get; set; }
+        private string HandleOrAlias { get; set; }
+        private int CodeathonWins { get; set; }
+
+        #endregion
+
+
+        #region Constructors
+
+        public HackersApplication(int CodingExperienceYears, string HandleOrAlias, int CodeathonWins,
+            string FirstName, string LastName, DateTime DateofBirth, string Address, string City, string State,
+            string Zip, string PhoneNumber, string Email)
+            : base(FirstName, LastName, DateofBirth, Address, City, State, Zip, PhoneNumber, Email)
+        {
+
+            this.CodingExperienceYears = CodingExperienceYears;
+            this.HandleOrAlias = HandleOrAlias;
+            this.CodeathonWins = CodeathonWins;
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        public override void Accept()
+        {
+            base.Accept();
+            Console.WriteLine($"{FirstName} {LastName} a.k.a. {HandleOrAlias}, You've been accepted to 'Hackers'. (*Accept() From Child Class - HackersApplication)");
+
+        }
+
+        public void Comment()
+        {
+
+            Console.WriteLine($"After contestant {FirstName} {LastName} a.k.a. {HandleOrAlias} logged into the website, " +
+                $"there seemed to be a system outage for the entire network. Hmmm...!?! And the lights went out!");
+
+        }
+
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab8_5RealityShowInheritanceComposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Lab8_5RealityShowInheritanceComposition!!");
+
+            // Testing the Classes
+            //  In the Main program, create applications for Dream House Hunters, Paradise Island,
+            //  and Above Deck. *Submit and accept them all*.
+
+            //  ContestantApplication contestantApp = new ContestantApplication("Omar", "Smith", Convert.ToDateTime("2/4/1975"), 
+            //                                                                "111 Bravo Way", "Los Angeles", "California", "90001",
+            //                                                                "888-233-1234", "omar.smith@hotmail.com");
+
+            DreamHouseHunterApplication houseApplication = new DreamHouseHunterApplication("Full Stack Web Developer", 100000, 6, 3, 2, "Omar", "Smith", Convert.ToDateTime("2/4/1975"),
+                                                                            "111 Bravo Way", "Los Angeles", "California", "90001",
+                                                                            "888-233-1234", "omar.smith@hotmail.com");
+            houseApplication.Submit();
+            houseApplication.Accept();
+
+            ParadiseIslandApplication paradiseApplication = new ParadiseIslandApplication("Female", "Carl", 2, "Patti", "Johnson", Convert.ToDateTime("8/4/1994"),
+                                                                            "211 Love Rd", "Los Angeles", "California", "90001",
+                                                                            "988-444-1234", "patti.johnson@gmail.com");
+            paradiseApplication.Submit();
+            paradiseApplication.Accept();
+
+            AboveDeckApplication deckApplication = new AboveDeckApplication(20, "American", "Captain", "Lee", Convert.ToDateTime("7/12/1964"),
+                                                                            "311 Bravo Way", "Los Angeles", "California", "90001",
+                                                                            "711-333-1234", "captain@gmail.com");
+            deckApplication.Submit();
+            deckApplication.Accept();
+
+            // The best Reality TV Show of the Decade - Hackers!
+            HackersApplication hackersApplication = new HackersApplication(5, "AlreadyGone", 25, "Bill", "Wright", Convert.ToDateTime("7/12/2000"),
+                                                                            "123 Hacker Lane", "Charlotte", "North Carolina", "28123",
+                                                                            "704-547-1234", "agimout@gmail.com");
+            hackersApplication.Submit();
+            hackersApplication.Accept();
+            hackersApplication.Comment(); // new wasn't in the courseware...added by th 07142020
+
+        }
+    }
+}
